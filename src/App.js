40a41e6182d54037d8e70c541c4518ccdc7d95ad
yen <japocoin@gmail.com>
2019-03-25 09:26:29 +0100

    before components

--- src/App.js
+++ src/App.js
@@ -10,32 +10,74 @@ class App extends Component {
 			inputMood : "",
 			inputMessage : "",
 			calendarMoodArray : []
+
 		}
 		this.getInputDate = this.getInputDate.bind(this);
 		this.getInputMood = this.getInputMood.bind(this);
 		this.getInputMessage = this.getInputDate.bind(this);
 	}
-	getInputDate (e){
+
+	componentDidMount (){
+
+	}
+	getInputDate = e =>{
 		const inputDate = e.currentTarget.value;
 		this.setState({
 			inputDate
 		})
 	}
-	getInputMood (e) {
+	getInputMood =e => {
 		const inputMood = e.currentTarget.value;
 		this.setState({
 			inputMood
 		})
 	}
 
-	getInputMessage (e) {
+	getInputMessage = e => {
 		const inputMessage = e.currentTarget.value;
 		this.setState({
 			inputMessage
 		})
 	}
+	checkRepeatedDate = () => {
+		const {calendarMoodArray, inputDate} =this.state;
+		for (const day of calendarMoodArray){
+			if (inputDate === day.date) {return true}
+		}
+	}
+
+	checkIfMoodSelected = () => {
+		const {inputMood} = this.state;
+		if (inputMood=== '') {return true}
+	}
+
+	saveDate = () => {
+		let {inputDate, inputMood, inputMessage} = this.state;
+		if (inputMood ==='badMood'){
+			inputMessage ='';
+		}
+		const newState ={
+			inputDate, inputMood, inputMessage
+		}
+		if (!this.checkRepeatedDate() && !this.checkIfMoodSelected()){
+			this.setState(prevState =>({
+				calendarMoodArray: [...prevState.calendarMoodArray, newState]
+			}))
+		} else {
+			alert ('You already add a mood for this day')
+		}
+	}
+
+	resetEditorInfo = () => {
+		this.setState({
+			inputDate: '',
+			inputMood: '',
+			inputMessage: ''
+		})
+	}
 
   render() {
+		const {getInputDate, getInputMood, getInputMessage, resetEditorInfo, saveDate}=this;
     return (
       <div className="App">
 			<section className="Editor">
@@ -43,25 +85,25 @@ class App extends Component {
 					<label htmlFor="date" className="editor__date--label">
 						Date:
 					</label>
-					<input id="date" type="date" className="editor__date--input" placeholder="21/04/2018" />
+					<input id="date" type="date" className="editor__date--input" placeholder="21/04/2018" onChange={getInputDate} />
 					<label htmlFor="mood" className="editor__mood__label">
 						Mood:
 					</label>
-					<input className="good__mood editor__mood--input" type="radio" name="good__mood" value=":)" />:)
-					<input className="bad_mood editor__mood--input" type="radio" name="bad__mood" value=":(" />:(
+					<input className="good__mood editor__mood--input" type="radio" name="good__mood" value=":)" onChange={getInputMood} />:)
+					<input className="bad_mood editor__mood--input" type="radio" name="bad__mood" value=":(" onChange = {getInputMood} />:(
 						<p className="textarea__title">Message:</p>
-						<textarea name="textarea" rows="2" cols="50" placeholder=" Why is such a good day?"></textarea>
+						<textarea name="textarea" rows="2" cols="50" placeholder=" Why is such a good day?" onKeyUp={getInputMessage}></textarea>
 				</div>
 				<div className="editor__button--container">
-						<button className="btn btn__save" type="button">Save</button>
-						<button className="btn btn__cancel" type="button">Cancel</button>
+						<button className="btn btn__save" type="button" onClick={saveDate}>Save</button>
+						<button className="btn btn__cancel" type="button" onClick={resetEditorInfo}>Cancel</button>
 				</div>
 			</section>
 			<section className ="Calendar">
 				<div className="Calendar__container"></div>
 			</section>
 			<section className="calendar">
-			<div className="calendar__add"> + </div>
+			<div className="calendar__add" onClick={resetEditorInfo}> + </div>
 			<div className="calendar__result--container">
 						<ul className="calendar__result">
 							<li className="calendar__result--face">:P</li>
